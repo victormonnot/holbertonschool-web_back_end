e669cb0309066b96d18702e6dd05db89bd4262b5
Victor Monnot <victormonnot@protonmail.com>
2026-04-09 15:57:59 +0200

    feat: Created all the files for the python_async_comprehension project

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Async Generator """
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """ Loop 10 times, wait 1 sec, yield random number 0-10 """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
