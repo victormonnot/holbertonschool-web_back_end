7334702962b059c3ab3e6ccff4536da95f3b3678
Victor Monnot <victormonnot@protonmail.com>
2026-04-09 16:03:13 +0200

    refactor: update async_generator return type hint to use Generator instead of AsyncGenerator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ Loop 10 times, wait 1 sec, yield random number 0-10 """
     for _ in range(10):
         await asyncio.sleep(1)
